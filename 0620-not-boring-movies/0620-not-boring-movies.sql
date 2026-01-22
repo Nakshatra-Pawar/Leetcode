abebef96303894c2d648e8aa7c7b133b26dac047
Nakshatra Pawar <109658269+Nakshatra-Pawar@users.noreply.github.com>
2026-01-22 14:51:29 -0800

    Time: 261 ms (18.61%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-SELECT * 
+SELECT *
 FROM Cinema
-WHERE id%2!=0 AND description NOT in('boring')
-ORDER BY rating desc;
+WHERE
+  MOD(id, 2) = 1
+  AND description != 'boring'
+ORDER BY rating DESC;
