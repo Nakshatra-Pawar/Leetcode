69408e8eae5da4ba7921f741498dd6c601afe079
Nakshatra Pawar <109658269+Nakshatra-Pawar@users.noreply.github.com>
2026-04-04 14:41:31 -0700

    Time: 0 ms (100%), Space: 20.4 MB (63.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        naks = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    naks.append(i)
-                    naks.append(j)
-                    return naks
-
-        return naks  
+        seen = {}
+        for i,n in enumerate(nums):
+            compliment = target - n
+            if compliment in seen:
+                return [seen[compliment], i]
+            else:
+                seen[n] = i
             
 
         
